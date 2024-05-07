663fcc302bcc97794483bafe17ab381afa0d79ef
CamCodes Baba <kbabazadeh@icloud.com>
2024-05-07 14:42:33 +0000

    Solve first 6 out of 13 Challenges
    1. I can provide my own url (gitpod, github)
    2. Only allow my site to be loaded in iFrames from my own page
    3. Dont allow DNS prefetching
    4. Only allow my site to be referrer for my own pages
    5. POST request to /api/threads/:board
    6. POST request to /api/replies/:board

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,97 @@
 'use strict';
 
+const boards = require("../data/data.js")
+const { v4: uuidv4 } = require('uuid');
+
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+  app.route('/api/threads/:board')
+    .get((req, res) => {
+      const boardName = req.params.board
+      res.json(boards[boardName].messages)
+    })
+    .post((req, res) => {
+      const boardName = req.params.board
+      console.log("trying to create a new board with a thread")
+      console.log(req.body, req.params)
+
+      //1. create message
+      let newMessage = {
+        _id: uuidv4(),
+        text: req.body.text,
+        created_on: new Date().toISOString(),
+        bumped_on: new Date().toISOString(),
+        delete_password: req.body.delete_password,
+        replies: [],
+        replycount: 0
+      }
+      //2. if board exists, apend message
+      if (boards[boardName]) {
+        boards[boardName].messages.push(newMessage)
+      } else {
+        //3. if its a new board, create it, and append message
+        let messageArray = []
+        messageArray.push(newMessage)
+        boards[boardName] = { messages: messageArray }
+      }
+
+      //4. redirect
+      res.redirect(`/b/${boardName}/`)
+    })
+
+  app.route('/api/replies/:board')
+    .get((req, res) => {
+
+      const boardName = req.params.board
+      const arrayToSearchThrough = boards[boardName].messages
+      const idToSearch = req.query.thread_id
+      let foundMessage = findMessage(arrayToSearchThrough, idToSearch)
+      res.json(foundMessage)
+    })
+    .post((req, res) => {
+      console.log("trying to create a reply to an existing message of an existing board")
+      console.log(req.body, req.params)
+
+      // 1. create reply
+      const replyDate = new Date().toISOString()
+      const reply = {
+        _id: uuidv4(),
+        text: req.body.text,
+        created_on: replyDate,
+        delete_password: req.body.delete_password,
+        reported: false,
+        thread_id: req.body.thread_id
+      }
+      // 2. add the reply to the according message
+      let arrayToSearchThrough = boards[req.params.board].messages
+      let idToSearch = req.body.thread_id
+      let foundMessage = findMessage(arrayToSearchThrough, idToSearch) // find the message
+      foundMessage.replies.push(reply) // append the reply
+
+      //3. Update the Message's bumped on date to right now
+      foundMessage.bumped_on = replyDate
+
+
+      // 4. redirect to /b/board/thread_id
+      res.redirect(`/b/${req.body.board}/${req.body.thread_id}`)
+    })
+
+
+
 
 };
+
+function findMessage(arrayToSearchThrough, idToSearch) {
+  let foundMessage = null
+  for (let i = 0; i < arrayToSearchThrough.length; i++) {
+    if (arrayToSearchThrough[i]._id === idToSearch) {
+      foundMessage = arrayToSearchThrough[i];
+      break;
+    }
+  }
+  if (foundMessage) {
+    // console.log(foundMessage);
+  } else {
+    console.log('Object not found');
+  }
+  return foundMessage
+}
--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 'use strict';
 require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet')
+const nocache = require('nocache')
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const boards = require("./data/data.js")
+
+
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 const app = express();
 
+app.use(
+  helmet({
+    referrerPolicy: {
+      policy: ["same-origin"],
+    },
+  })
+);
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: 'sameorigin' }));
+app.use(helmet.xssFilter())
+app.use(helmet.noSniff())
+app.use(helmet.ieNoOpen())
+app.use(helmet.dnsPrefetchControl())
+app.use(nocache())
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -40,7 +60,7 @@ fccTestingRoutes(app);
 apiRoutes(app);
 
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
@@ -49,12 +69,12 @@ app.use(function(req, res, next) {
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         console.log('Tests are not valid:');
         console.error(e);
       }
